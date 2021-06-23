ed92104ba9fec0e7fafa184aed8e39aa5bf306e9
Randy Seng <rseng@pm.me>
2021-06-23 18:14:47 +0200

    Add automated acquisition of osm data from geofabrik

---
+++ mobitopp_preprocessing/helpers/url_helper.py
@@ -0,0 +1,22 @@
+import requests 
+import urllib.request
+
+def extractFileNameFromUrl(url):
+    return url.split('/')[-1]
+
+
+def getDataFromUrl(url):
+    response = requests.get(url)
+    return response.text
+
+
+def urlExist(url):
+    status_code = requests.head(url).status_code
+    if (status_code < 400):
+        return False
+    else:
+        return True
+
+
+def downloadFromUrl(url, pathToFile):
+    urllib.request.urlretrieve(url, pathToFile)
