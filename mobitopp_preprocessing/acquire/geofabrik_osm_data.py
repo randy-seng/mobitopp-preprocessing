5f94bdce66c22873a9062d6c480eaebd4e34e585
Randy Seng <rseng@pm.me>
2021-06-24 20:12:58 +0200

    Remove trailing whitespaces when reading from download_urls.txt

--- mobitopp_preprocessing/acquire/geofabrik_osm_data.py
+++ mobitopp_preprocessing/acquire/geofabrik_osm_data.py
@@ -8,6 +8,7 @@ from mobitopp_preprocessing.helpers import file_helper
 from mobitopp_preprocessing.helpers import url_helper
 from mobitopp_preprocessing.helpers import md5_helper
 
+
 def downloadFromURL(url, dirPath):
     '''Download from url
 
@@ -20,16 +21,14 @@ def downloadFromURL(url, dirPath):
     pathToFile = os.path.join(dirPath, fileName)
 
     # TODO: does dirPath only specify directory or the whole file
-    if (url_helper.urlExist(url)):
-        print("Download failed! Please check that the url is working: {}".format(url))
+    if (not url_helper.urlExist(url)):
+        print("URL is not working! Please check the url: {}"
+              .format(url))
         return False
-
 
     if (file_helper.fileAlreadyExists(pathToFile)):
         # TODO: make method to extract the hash from an md5 file
-        urlMD5 = url + ".md5"
-        urlDotMD5File= url_helper.getDataFromUrl(urlMD5)
-        #urlMD5File = "http://download.geofabrik.de/south-america/suriname-latest.osm.pbf.md5"
+        urlDotMD5File = url_helper.getDataFromUrl(url + ".md5")
         fileHash = md5_helper.createHash(pathToFile)
         urlHash = md5_helper.getHashFromMD5File(urlDotMD5File)
         # TODO: Check if it really gets an .md5 file containing the hash
@@ -42,14 +41,12 @@ def downloadFromURL(url, dirPath):
             newFileNamePath = appendTimestamp(pathToFile)
             url_helper.downloadFromUrl(url, newFileNamePath)
             return True
-            
-
     else:
         # File not downloaded yet
         print("Downloading: " + fileName)
         urllib.request.urlretrieve(url, pathToFile)
         url_helper.downloadFromUrl(url, pathToFile)
-        print("SUCCESS!")
+        print("SUCCESS! {} downloaded!".format(fileName))
         return True
 
 
@@ -59,34 +56,40 @@ def appendTimestamp(str):
 
 
     :param str: a string
-    :returns: the string appended with the current timestamp 
+    :returns: the string appended with the current timestamp
     '''
     date = datetime.now()
     timestamp = date.strftime("-%Y-%m-%dT%H:%M:%S")
     return str + timestamp
 
+
 def downloadFromTextFileParallel(dirPath, pathToTextFile):
     textFile = file_helper.readTextFile(pathToTextFile)
-    argList = zip(textFile, itertools.repeat(dirPath))  # [(url, dirPath)]
+    textWithoutLF = removeTrailingLF(textFile)
+    argList = zip(textWithoutLF, itertools.repeat(dirPath))  # [(url, dirPath)]
     # Initialise and assign tasks to worker pool
     pool = mp.Pool(mp.cpu_count())
     pool.starmap(downloadFromURL, argList)
     pool.close()
 
-    
+
+def removeTrailingLF(strList):
+    return list(map(str.rstrip, strList))
 
 
 def main():
-    url = "https://download.geofabrik.de/antarctica-latest.osm.pbf"
-    fileLocation = Path(__file__).parents[2] / "data" / "osm"
+    downloadLocation = Path(__file__).parents[2] / "data" / "osm" /"pbf_files"
+
+    if not os.path.isdir(downloadLocation):
+        os.makedirs(downloadLocation)
 
     # download Antarctica dataset
     # downloadFromURL(url, os.path.join(fileLocation, fileLocation))
 
     # download dataset from text file
-    pathToTextFile = Path(__file__).parents[0] / "resources" / "download_urls.txt"
+    pathToTextFile = Path(__file__).parents[0] / "resources/download_urls.txt"
     # downloadFromTextFile(url, fileLocation, pathToFile)
-    downloadFromTextFileParallel(fileLocation, pathToTextFile)
+    downloadFromTextFileParallel(downloadLocation, pathToTextFile)
 
 
 if __name__ == "__main__":
