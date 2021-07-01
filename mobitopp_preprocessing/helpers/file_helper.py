469208c3b3b3174ff84456907103c79c86b9fedf
Randy Seng <rseng@pm.me>
2021-07-01 02:42:23 +0200

    Add directory and file creation helper functions

--- mobitopp_preprocessing/helpers/file_helper.py
+++ mobitopp_preprocessing/helpers/file_helper.py
@@ -1,6 +1,11 @@
 from pathlib import Path
+import os
+
 
 def readTextFile(pathToTextFile):
+    ''' Returns content of a file.
+
+    '''
     with open(pathToTextFile) as f:
         textFile = f.readlines()
         f.close()
@@ -11,3 +16,20 @@ def readTextFile(pathToTextFile):
 def fileAlreadyExists(pathToFile):
     return Path(pathToFile).is_file()
 
+
+def createFile(pathToDir, filename):
+    ''' Creates file if it doesn't exist.
+
+    '''
+    file = os.path.join(pathToDir, filename)
+    createDir(pathToDir)
+
+    if not os.path.exists(file):
+        open(file, 'w').close()
+
+
+def createDir(pathToDir):
+    ''' Creates directory, if it doesn't exists.
+
+    '''
+    Path(pathToDir).mkdir(exist_ok=True)
