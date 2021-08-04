5d84b27c02d4a4d33dc6c8d346cbebc053b7cdfa
Randy Seng <rseng@pm.me>
2021-08-05 01:02:18 +0200

    Refactor

--- mobitopp_preprocessing/acquire/geofabrik_osm_data.py
+++ mobitopp_preprocessing/acquire/geofabrik_osm_data.py
@@ -2,37 +2,38 @@ import urllib.request
 import itertools
 import os
 import multiprocessing as mp
-from pathlib import Path
 from datetime import datetime
+
+from pathlib import Path
+
 from mobitopp_preprocessing.helpers import file_helper
 from mobitopp_preprocessing.helpers import url_helper
 from mobitopp_preprocessing.helpers import md5_helper
 
 
 def downloadFromURL(url, dirPath):
-    '''Download from url
+    """Download from url
 
 
     :param url: the url to download the data from
     :param dirPath: the path to the directory to save the data in
     :returns: True on SUCCESS, False on FAIL
-    '''
+    """
     fileName = url_helper.extractFileNameFromUrl(url)
     pathToFile = os.path.join(dirPath, fileName)
 
     # TODO: does dirPath only specify directory or the whole file
-    if (not url_helper.urlExist(url)):
-        print("URL is not working! Please check the url: {}"
-              .format(url))
+    if not url_helper.urlExist(url):
+        print("URL is not working! Please check the url: {}".format(url))
         return False
 
-    if (file_helper.fileAlreadyExists(pathToFile)):
+    if file_helper.file_exists(pathToFile):
         # TODO: make method to extract the hash from an md5 file
         urlDotMD5File = url_helper.getDataFromUrl(url + ".md5")
         fileHash = md5_helper.createHash(pathToFile)
         urlHash = md5_helper.getHashFromMD5File(urlDotMD5File)
         # TODO: Check if it really gets an .md5 file containing the hash
-        if (md5_helper.hashesAreEqual(fileHash, urlHash)):
+        if md5_helper.hashesAreEqual(fileHash, urlHash):
             # file already downloaded
             print("File from {} already downloaded!".format(url))
             return False
@@ -52,12 +53,12 @@ def downloadFromURL(url, dirPath):
 
 # TODO: Create test
 def appendTimestamp(str):
-    '''
+    """
 
 
     :param str: a string
     :returns: the string appended with the current timestamp
-    '''
+    """
     date = datetime.now()
     timestamp = date.strftime("-%Y-%m-%dT%H:%M:%S")
     return str + timestamp
@@ -78,7 +79,7 @@ def removeTrailingLF(strList):
 
 
 def main():
-    downloadLocation = Path(__file__).parents[2] / "data" / "osm" /"pbf_files"
+    downloadLocation = Path(__file__).parents[2] / "data" / "osm" / "pbf_files"
 
     if not os.path.isdir(downloadLocation):
         os.makedirs(downloadLocation)
