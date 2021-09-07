6b92cda30fc5fcc2aa40e3f995b286513f119baf
Randy Seng <rseng@pm.me>
2021-09-08 01:19:33 +0200

    Add config

---
+++ mobitopp_preprocessing/preprocessing/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+URBAN_ROAD_NETWORK_CFG = os.path.join(
+    os.getcwd(),
+    "mobitopp_preprocessing/preprocessing/resources/road_network/Urban road network/Urban road network.cfg",
+)
+URBAN_ROAD_NETWORK_DEFAULT = os.path.join(
+    os.getcwd(),
+    "mobitopp_preprocessing/preprocessing/resources/road_network/urban_road_network_defaults.json",
+)
