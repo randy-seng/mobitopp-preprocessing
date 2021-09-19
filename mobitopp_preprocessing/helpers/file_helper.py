0ad691237b85a9f5473af40f105495dcf289c32d
Randy Seng <rseng@pm.me>
2021-09-19 23:52:39 +0200

    Add .json extension

--- mobitopp_preprocessing/helpers/file_helper.py
+++ mobitopp_preprocessing/helpers/file_helper.py
@@ -21,7 +21,7 @@ def read_json_file(path):
 
 
 def save_as_json_file(dir_path, filename, data):
-    path = os.path.join(dir_path, filename)
+    path = os.path.join(dir_path, filename + ".json")
     createFile(dir_path, filename)
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
